5d2d5924ad24ad1b813d0d637947d0288b66d4e3
tuananh-ngx <tuananhngx.work@gmail.com>
2023-10-12 11:41:29 +0700

    1st upload

---
+++ src/main/java/com/ngtuananh/mathutil/usage/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ngtuananh.mathutil.usage.main;
+
+import com.ngtuananh.mathutil.core.MathUtility;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("6! = (720)" + MathUtility.getFactorial(6));
+    }
+}
